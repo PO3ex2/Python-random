78c3e9199c7b3eb2ca3440bf1669a1dff7f4ddae
Ruslan Anisimov <ruslan.anisimov024@skolavdf.cz>
2024-12-19 12:28:58 +0100

    Example commit

---
+++ snowman.py
@@ -0,0 +1,10 @@
+ballsAmount = int(input("Zadejte počet koulí sněhulaka: ")) - 2
+print("  _  ")
+print(" /_\ ")
+print("(ಠ_ಠ)")
+
+while ballsAmount != 0 :
+    print("( . )")
+    ballsAmount -= 1
+
+print("(_ _)")
